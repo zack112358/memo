f634fd01191d907850950934c860f0553ea63556
Zachary McCord <zjmccord@gmail.com>
2014-06-26 17:02:43 -0400

    doc't & tests

--- memo.py
+++ memo.py
@@ -9,6 +9,19 @@ import unittest
 
 
 def memoize(table_factory):
+    """
+    Memoization decorator.
+
+    @memoize(dict)
+    def fib(n):
+        return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
+    Argument to decorator is a callable that produces a dict-like thing to
+    memoize the function results with.
+
+    Works on methods, too, but you have to put it *after* the @classmethod or
+    @staticmethod decorators, if any.
+    """
     return functools.partial(_memoize, table_factory=table_factory)
 
 
@@ -53,3 +66,50 @@ class MemoizeTest(unittest.TestCase):
         
         self.assertEqual(fib(5), 8)
         self.assertEqual(calls, 6)
+
+    def test_methods(self):
+        global calls
+        calls = 0
+        class George(object):
+            def __init__(self, n):
+                self.n = n
+
+            @memoize(dict)
+            def fred(self):
+                global calls
+                calls += 1
+                return self.n
+
+            @classmethod
+            @memoize(dict)
+            def quux(cls):
+                global calls
+                calls += 1
+                return "quux"
+            
+            @staticmethod
+            @memoize(dict)
+            def quuxley():
+                global calls
+                calls += 1
+                return "quuxley"
+
+        george_1 = George(1)
+        george_2 = George(2)
+        self.assertEqual(george_1.fred(), 1)
+        self.assertEqual(george_2.fred(), 2)
+        self.assertEqual(george_1.fred(), 1)
+        self.assertEqual(george_2.fred(), 2)
+        self.assertEqual(calls, 2)
+
+        calls = 0
+        self.assertEqual(George.quux(), "quux")
+        self.assertEqual(george_1.quux(), "quux")
+        self.assertEqual(george_2.quux(), "quux")
+        self.assertEqual(calls, 1)
+
+        calls = 0
+        self.assertEqual(George.quuxley(), "quuxley")
+        self.assertEqual(george_1.quuxley(), "quuxley")
+        self.assertEqual(george_2.quuxley(), "quuxley")
+        self.assertEqual(calls, 1)
